78a42cfb7b24036bb6bab7f774e5685c5d98f55f
Fareed Mohammad Qararyah <qarayah@c21vbll.lan>
2021-07-29 12:50:08 +0200

    add_prepro

--- BenchmarkModel.py
+++ BenchmarkModel.py
@@ -34,28 +34,33 @@ class BenchmarkModel:
                             from_logits=True),
                         metrics=['accuracy'])
         
-        test_images = test_images / 255.0
-        test_images = test_images[0:max(self.batch_sizes) * 10]
+        test_images_preprocessed = test_images / 255.0
+        test_images_preprocessed = test_images_preprocessed[0:max(self.batch_sizes) * 10]
         with open(Settings.Settings().metrics_file + '_' +self.model_name + '_' +str(datetime.datetime.now()).split('.')[0], 'w') as f:
             for batch_size in self.batch_sizes:
                 f.write('\n----------------\nbatch size: ' + str(batch_size) + '\n----------------\n')
                 #Throughput
                 t0 = time.time()
                 #test_loss, test_acc = pretrained_model.evaluate(test_images,  test_labels)#, verbose=2)
-                tmp = np.argmax(self.pretrained_model.predict(x = test_images, batch_size = batch_size, verbose = 0))
-                f.write("Throughput is: " + str(len(test_images) / (time.time() - t0)) + " images per second.\n")
+                tmp = np.argmax(self.pretrained_model.predict(x = test_images_preprocessed, batch_size = batch_size, verbose = 0))
+                f.write("Throughput is: " + str(len(test_images_preprocessed) / (time.time() - t0)) + " images per second.\n")
                 #end throughput
 
                 #latency
                 avg_time = 0.0
+                avg_time_with_preprocessing = 0.0
                 counter = 0
                 while counter * batch_size < max(self.batch_sizes) * 10:
-                    image = test_images[counter * batch_size: (counter + 1) * batch_size + 1]
+                    image_batch = test_images[counter * batch_size: (counter + 1) * batch_size + 1]
+                    t0_with_preprocessing = time.time()
+                    image_batch = image_batch / 255.0
                     t0 = time.time()
-                    tmp = np.argmax(self.pretrained_model.predict(x = image, batch_size = batch_size, verbose = 0))
+                    tmp = np.argmax(self.pretrained_model.predict(x = image_batch, batch_size = batch_size, verbose = 0))
                     avg_time += time.time() - t0
+                    avg_time_with_preprocessing += time.time() - t0_with_preprocessing
                     counter += 1
 
                 avg_time /= counter
-                f.write("Latency is: " + str(avg_time) + " seconds.\n")
+                f.write("Latency (with processing time) is: " + str(avg_time) + " seconds.\n")
+                f.write("Latency (without processing time) is: " + str(avg_time_with_preprocessing) + " seconds.\n")
                 #end latency
