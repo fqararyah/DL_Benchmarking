af6d18da1f7cf68f01de84dbafd0b5fd71986a59
fareed <fqararyah@gmail.com>
2021-08-08 12:26:47 +0200

    first load the model

--- BenchmarkModel.py
+++ BenchmarkModel.py
@@ -72,11 +72,14 @@ class BenchmarkModel:
             str(input_dim[1]) + '_' + str(datetime.datetime.now()).split('.')[0], 'w') as f:
             for batch_size in self.batch_sizes:
                 f.write('\n----------------\nbatch size: ' + str(batch_size) + '\n----------------\n')
+                #this is to load the model
+                tmp = np.argmax(self.pretrained_model.predict(x = test_images[max(self.batch_sizes)*10:max(self.batch_sizes)*20]/255.0, batch_size = \
+                    batch_size, verbose = 0))
                 #Throughput
-                #t0 = time.time()
+                t0 = time.time()
                 #test_loss, test_acc = pretrained_model.evaluate(test_images,  test_labels)#, verbose=2)
-                #tmp = np.argmax(self.pretrained_model.predict(x = test_images_preprocessed, batch_size = batch_size, verbose = 0))
-                #f.write("Execution time is: " + str((time.time() - t0) / len(test_images_preprocessed)) + "seconds.\n")
+                tmp = np.argmax(self.pretrained_model.predict(x = test_images_preprocessed, batch_size = batch_size, verbose = 0))
+                f.write("Execution time is: " + str((time.time() - t0) / len(test_images_preprocessed)) + "seconds.\n")
                 #end throughput
 
                 #latency
@@ -86,9 +89,9 @@ class BenchmarkModel:
                 while counter * batch_size < max(self.batch_sizes) * 10:
                     image_batch = test_images[counter * batch_size: (counter + 1) * batch_size]
                     #t0_with_preprocessing = time.time()
-                    image_batch = image_batch / 255.0
                     tmp = -1
                     t0 = time.time()
+                    image_batch = image_batch / 255.0
                     tmp = np.argmax(self.pretrained_model.predict(x = image_batch, batch_size = batch_size, verbose = 0))
                     if tmp != -1:
                         avg_time += time.time() - t0
@@ -112,28 +115,29 @@ class BenchmarkModel:
                 f.write('\n----------------\nbatch size: ' + str(batch_size) + '\n----------------\n')
                 #latency
                 avg_latency = 0
+                avg_time = 0
                 #avg_time_with_preprocessing = 0.0
                 counter = 0
+                interpreter.resize_tensor_input(0,[batch_size, 32, 32, 3])
+                interpreter.allocate_tensors()
                 while counter * batch_size < max(self.batch_sizes) * 10:
                     image_batch = test_images[counter * batch_size: (counter + 1) * batch_size]
-                    interpreter.resize_tensor_input(0,[batch_size, 32, 32, 3])
-                    #t0_with_preprocessing = time.time()
-                    image_batch = image_batch / 255.0
-                    interpreter.allocate_tensors()
                     t0 = time.time()
+                    image_batch = image_batch / 255.0
                     interpreter.set_tensor(input_index, image_batch.astype(np.float32))
                     t1 = time.time()
                     interpreter.invoke()
                     predictions = interpreter.get_tensor(output_index)
                     predicted = arg_max(predictions, 0)
                     if len(predicted) > 0:
+                        avg_time += time.time() - t1
                         avg_latency += time.time() - t0
                     #avg_time_with_preprocessing += time.time() - t0_with_preprocessing
                     counter += 1
-                    #avg_time /= len(test_images_preprocessed)
+                avg_time /= len(test_images_preprocessed)
                 avg_latency /= len(test_images_preprocessed)
                 #avg_time_with_preprocessing /= counter
-                #f.write("Execution time is: " + str(avg_time) + " seconds.\n")
+                f.write("Execution time is: " + str(avg_time) + " seconds.\n")
                 f.write("Latency is: " + str(avg_latency) + " seconds.\n")
                 #f.write("Latency (with processing time) is: " + str(avg_time_with_preprocessing) + " seconds.\n")
                 #end latency
