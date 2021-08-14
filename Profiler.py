1beb5cfcde195d86e09eada22b7a779101168546
fareed <fqararyah@gmail.com>
2021-08-14 17:16:59 +0200

    a

--- Profiler.py
+++ Profiler.py
@@ -1,4 +1,3 @@
-from TegraProfiler import TegraProfiler
 import Settings
 import os
 
@@ -16,10 +15,3 @@ class Profiler:
             for line in f:
                 line = line.replace(' ', '').replace('\n', '')
                 self.profiling_description = line
-
-settings_obj = Settings.Settings()
-device_name = settings_obj.device_name.lower()
-if settings_obj.power_profile  == 1:
-    if 'tegra' in device_name or 'jetson' in device_name:
-        profiler = TegraProfiler(settings_obj, device_name)
-        profiler.profile()
---
+++ profile_power.py
@@ -0,0 +1,9 @@
+import Settings
+import TegraProfiler
+
+settings_obj = Settings.Settings()
+device_name = settings_obj.device_name.lower()
+if settings_obj.power_profile  == 1:
+    if 'tegra' in device_name or 'jetson' in device_name:
+        profiler = TegraProfiler(settings_obj, device_name)
+        profiler.profile()
