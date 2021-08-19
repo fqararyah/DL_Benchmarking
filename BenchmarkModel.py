f1de938d78a3e751767afa030ea40abe1620bf17
fareed <fqararyah@gmail.com>
2021-08-19 13:53:56 +0200

    memory

--- BenchmarkModel.py
+++ BenchmarkModel.py
@@ -35,12 +35,13 @@ class BenchmarkModel:
         self.num_classes = num_classes
 
     def get_metrics(self):
-        physical_devices = tf.config.list_physical_devices('GPU')
-        try:
-            tf.config.experimental.set_memory_growth(physical_devices[0], True)
-        except:
-        # Invalid device or cannot modify virtual devices once initialized.
-            pass
+        if Settings.Settings().power_profile:
+            physical_devices = tf.config.list_physical_devices('GPU')
+            try:
+                tf.config.experimental.set_memory_growth(physical_devices[0], True)
+            except:
+            # Invalid device or cannot modify virtual devices once initialized.
+                pass
         #load_images()
         for input_dim in self.inputs_dims:
             test_images = np.random.randint(low =0, high= 256, size = [1000, input_dim[0], input_dim[1],\
--- clean_tegra_prof.py
+++ clean_tegra_prof.py
@@ -16,12 +16,12 @@ with open(in_file, 'r') as f:
             if '*' not in splits[0] and len(splits[0]):
                 current_key = splits[0]
                 if current_key not in power_dict:
-                    power_dict[current_key] = [{'VDD_SYS_GPU': 0,'VDD_SYS_SOC': 0, 'VDD_IN': 0, 'VDD_SYS_DDR': 0}, 0]
+                    power_dict[current_key] = [{'VDD_SYS_GPU': 0,'VDD_SYS_SOC': 0, 'VDD_IN': 0, 'VDD_SYS_DDR': 0, 'RAM': 0.0}, 0]
         else:
             for i in range(len(splits)):
                 if splits[i] in power_dict[current_key][0]:
                     power_dict[current_key][0][splits[i]] += int(splits[i + 1].split('/')[0])
-                    power_dict[current_key][1] += 1
+            power_dict[current_key][1] += 1
 
 for key, val in power_dict.items():
     for part_pow_cons_key, part_pow_cons_val in val[0].items():
