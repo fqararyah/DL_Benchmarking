b850d62b0d347d2a0c1d6a22f9fa7b3ca9b741ef
fareed <fqararyah@gmail.com>
2021-09-06 12:11:53 +0200

    a

---
+++ other/one_infer.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras import datasets
+import ssl
+import time
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+pretrained_model = tf.keras.applications.MobileNetV3Small()
+
+# Normalize pixel values to be between 0 and 1
+test_images = np.random.randint(low =0, high= 256, size = [1, 224, 224,\
+                 3], dtype=np.uint8)
+
+
+tmp = np.argmax(pretrained_model(test_images, training = False))
+
+print(tmp)
