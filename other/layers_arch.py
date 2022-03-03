bb2ca4af5414c9da6dd2a976e2f863eeb6df7596
fareed <fqararyah@gmail.com>
2022-03-03 14:59:33 +0100

    a

--- other/layers_arch.py
+++ other/layers_arch.py
@@ -20,20 +20,22 @@ for layer in range(0, len(pretrained_model.layers)):
     #print(pretrained_model.layers[layer].get_config())
     current_layer = pretrained_model.layers[layer]
 
-    if '_conv' in current_layer.name.lower():
-        print(current_layer.kernel_size)
-    elif 'bn' in current_layer.name:
-        print(current_layer.weights[0].numpy().shape)
-        print(current_layer.axis)
-        print(current_layer.momentum)
-        print(current_layer.epsilon)
-        print(current_layer.center)
-        print(current_layer.scale)
-        print(current_layer.beta_initializer)
-        print(current_layer.gamma_initializer)
-        print(current_layer.moving_mean_initializer)
-        print(current_layer.moving_variance_initializer)
-        print(current_layer.beta_regularizer)
-        print(current_layer.gamma_regularizer)
-        print(current_layer.beta_constraint)
-        print(current_layer.gamma_constraint)
+    if 'conv' in current_layer.name.lower() and not '_bn' in current_layer.name.lower() and \
+        not '_relu' in current_layer.name.lower() and current_layer.weights:
+        print('conv: ', current_layer.name, current_layer.weights[0].shape)
+    # elif 'bn' in current_layer.name:
+    #     print('bn: ')
+    #     print(current_layer.weights[0].numpy().shape)
+    #     print(current_layer.axis)
+    #     print(current_layer.momentum)
+    #     print(current_layer.epsilon)
+    #     print(current_layer.center)
+    #     print(current_layer.scale)
+    #     print(current_layer.beta_initializer)
+    #     print(current_layer.gamma_initializer)
+    #     print(current_layer.moving_mean_initializer)
+    #     print(current_layer.moving_variance_initializer)
+    #     print(current_layer.beta_regularizer)
+    #     print(current_layer.gamma_regularizer)
+    #     print(current_layer.beta_constraint)
+    #     print(current_layer.gamma_constraint)
