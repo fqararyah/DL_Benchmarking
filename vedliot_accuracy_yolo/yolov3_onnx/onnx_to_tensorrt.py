80ea63be1eb857f7fc70d246319eb1cd19c1f796
fqararyah@gmail.com <fqararyah@gmail.com>
2022-08-30 19:55:53 +0200

    vedliot_accuracy

--- vedliot_accuracy_yolo/yolov3_onnx/onnx_to_tensorrt.py
+++ vedliot_accuracy_yolo/yolov3_onnx/onnx_to_tensorrt.py
@@ -192,7 +192,7 @@ def main():
                     (156, 198),
                     (373, 326),
                 ],
-                "obj_threshold": 0.00001,  # Threshold for object coverage, float value between 0 and 1 , fareed: it was 0.6
+                "obj_threshold": 0.000000001,  # Threshold for object coverage, float value between 0 and 1 , fareed: it was 0.6
                 "nms_threshold": 0.5,  # Threshold for non-max suppression algorithm, float value between 0 and 1, , fareed: it was 0.5
                 "yolo_input_resolution": input_resolution_yolov3_HW,
             }
